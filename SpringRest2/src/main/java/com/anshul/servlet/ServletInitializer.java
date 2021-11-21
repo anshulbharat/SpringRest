48bfc43634e4cf6ea67597ce29ca413250e52fc6
anshulbh <anshulbh@LIN51017892.corp.capgemini.com>
2021-11-21 18:50:23 +0530

    SR2

---
+++ SpringRest2/src/main/java/com/anshul/servlet/ServletInitializer.java
@@ -0,0 +1,15 @@
+package com.anshul.servlet;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.anshul.Main.RestAuthenticateUserApplication;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestAuthenticateUserApplication.class);
+	}
+
+}
